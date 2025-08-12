800b2196f79765ed1d5b64ec0a5337a684eeba3d
Tridib Roy Chowdhury <tridibroychowdhury9@gmail.com>
2025-08-12 23:46:19 +0530

    API KEY INTEGRATION

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import dill
 import spacy
+from dotenv import load_dotenv
+import google.generativeai as genai
+import os
+load_dotenv()
 
 nlp = spacy.load("en_core_web_sm") 
 
@@ -13,7 +17,21 @@ extract_key_points = loaded_functions['extract_key_points']
 is_coding_related_bow = loaded_functions['is_coding_related_bow']
 is_general_question = loaded_functions['is_general_question']
 
-sentence = "Write an C program to calculate the factorial of a number"
+sentence = "What essay on C language evaluation?"
 
 model_train(sentence)
 print(model_train)
+api_key=""
+if(model_train=='Claude'):
+    api_key=os.getenv('CLAUDE_KEY')
+elif(model_train=='Gemini'):
+    api_key=os.getenv('GEMINI_KEY')
+else:
+    api_key=os.getenv('PERPLEXITY_KEY')
+
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+response = model.generate_content(sentence)
+print(response.text)
+
+
